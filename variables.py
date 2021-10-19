f4b40ee574a7f57d9b40d394c81c996fed1c275e
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2021-10-19 13:18:46 +0100

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,44 @@
+# variable is a container for a value, which can be of various types
+'''
+this is a 
+multiline comment
+or docstring (used to define a function purpose)
+can be single or double quotes
+'''
+"""
+VARIABLE RULES
+    -Variable names are case sensitive (Name nd name are different variables)
+    - Must start with a letter or an underscore 
+    - Can have numbers but can not start with one 
+"""
+#x = 1        # int
+#y = 2.5        # float
+#name = 'john'  # str
+#is_cool = True #bool
+#print(x , y, name)
+
+# multiple assignment 
+x , y, name, is_cool = (1,2.5,'john',True)
+
+#print(x , y, name)
+
+# Basic math 
+a = x + y
+
+# Casting 
+x = str(x)
+y = int(y)
+z = float(y)
+print(type(z),z)
+
+# create a veriable A B and C
+# asign a number to it 
+# get the sum of A and B 
+# assign a variable to the sum
+# get the remainder of the division of B and C
+# assign a variable to the remainder 
+# multiply the sum(A$B) with the remainder(B$C)
+# assign a variable to the multiplication
+# print the type of the multiplication 
+
+
